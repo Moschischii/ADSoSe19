e680bca424ddfc46783ef22eb22e8667b9b62bbd
Sebastian Geitner <sebastian.geitner@gmail.com>
2019-03-27 12:38:11 +0100

    Messen von Zeit und zählen der Aufrufe

--- Uebungsblatt1/Aufgabe4/Matrix.java
+++ Uebungsblatt1/Aufgabe4/Matrix.java
@@ -1,5 +1,6 @@
 package Uebungsblatt1.Aufgabe4;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Matrix {
@@ -7,8 +8,12 @@ public class Matrix {
     private int m;
     private int n;
     private int[][] matrix;
+    public long zeit;
+    public long zaehler;
 
     Matrix(int m, int n) {
+        this.zaehler = 0;
+        this.zeit = 0;
         this.m = m;
         this.n = n;
         matrix = new int[m][n];
@@ -18,6 +23,16 @@ public class Matrix {
         for (int i = 0; i < this.m; i++) {
             for (int j = 0; j < this.n; j++) {
                 this.matrix[i][j] = 0;
+            }
+        }
+    }
+
+    public void initRandom() {
+        Random rand = new Random();
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                this.matrix[i][j] = 3;
+                // this.matrix[i][j] = rand.nextInt(10);
             }
         }
     }
@@ -42,17 +57,26 @@ public class Matrix {
     }
 
     public Matrix add(Matrix M) {
+        this.zaehler = 0;
         if (M.m != this.m || M.n != this.n) {
             System.out.println("Matrix hat nicht die gleiche Größe!");
             return M;
         }
         else {
             Matrix out = new Matrix(this.m, this.n);
+            out.zaehler = 0;
+            out.zeit = 0;
+            final long timeStart = System.currentTimeMillis();
             for (int i = 0; i < out.m; i++) {
                 for (int j = 0; j < out.n; j++) {
+                    out.zaehler ++;
                     out.matrix[i][j] = (M.matrix[i][j] + this.matrix[i][j]);
                 }
             }
+            final long timeEnd = System.currentTimeMillis();
+            out.zeit = (timeEnd - timeStart);
+            System.out.println("Time = " + out.zeit + " ms");
+            System.out.println("Aufrufe = " + out.zaehler);
             return out;
         }
     }
@@ -60,12 +84,21 @@ public class Matrix {
     public Matrix mult(Matrix M) {
        if (this.n == M.m) {
            Matrix out = new Matrix(this.m,M.n);
+           out.zaehler = 0;
+           out.zeit = 0;
+           final long timeStart = System.currentTimeMillis();
            for (int i = 0; i < out.m; i++) {
                for (int j = 0; j < out.n; j++) {
-                   for (int k = 0; k < this.n; k++)
+                   for (int k = 0; k < this.n; k++) {
+                       out.zaehler++;
                        out.matrix[i][j] += (this.matrix[i][k] * M.matrix[k][j]);
+                   }
                }
            }
+           final long timeEnd = System.currentTimeMillis();
+           out.zeit = (timeEnd - timeStart);
+           System.out.println("Time = " + out.zeit + " ms");
+           System.out.println("Aufrufe = " + out.zaehler);
            return out;
        }
        else {
--- Uebungsblatt1/Aufgabe4/aufgabe4.java
+++ Uebungsblatt1/Aufgabe4/aufgabe4.java
@@ -2,36 +2,45 @@ package Uebungsblatt1.Aufgabe4;
 
 public class aufgabe4 {
     public static void main(String[] args) {
-        int m = 0, n = 0;
-        add(m,n);
-        mult(m,n);
+        int m = 2000, n = 2000;
+        int maxTime = 60; // max time in sek
+        System.out.println("Start");
+
+        /*
+        while (true) {
+            m = m + 10;
+            n = n + 10;
+            long multTime = mult(m,n);
+            System.out.println("M = " + m + " N = " + n + " Zeit = " + (multTime/1000) + " sek");
+            if (multTime/1000 > maxTime) {
+                System.out.println("Ergebnis Mult: M = " + m + " N = " + n + " Zeit = " + (multTime/1000) + " sek");
+                break;
+            }
+        }
+        */
+
+        long addTime = add(m,n);
+        long multTime = mult(m,n);
+        System.out.println("Ergebnis Add: M = " + m + " N = " + n + " Zeit = " + (addTime/1000) + " sek");
+        System.out.println("Ergebnis Mult: M = " + m + " N = " + n + " Zeit = " + (multTime/1000) + " sek");
+        System.out.println("End");
     }
 
-    private static void add(int m, int n) {
-        System.out.println("Matrix 1:");
+    private static long add(int m, int n) {
         Matrix matrix1 = new Matrix(m,n);
-        matrix1.init();
-        matrix1.input();
-        System.out.println("Matrix 2:");
+        matrix1.initRandom();
         Matrix matrix2 = new Matrix(m,n);
-        matrix2.init();
-        matrix2.input();
-        System.out.println("Add:");
+        matrix2.initRandom();
         Matrix matrixNew = matrix1.add(matrix2);
-        matrixNew.print();
+        return matrixNew.zeit;
     }
 
-    private static void mult(int m, int n) {
-        System.out.println("Matrix 1:");
+    private static long mult(int m, int n) {
         Matrix matrix1 = new Matrix(m,n);
-        matrix1.init();
-        matrix1.input();
-        System.out.println("Matrix 2:");
+        matrix1.initRandom();
         Matrix matrix2 = new Matrix(n,m);
-        matrix2.init();
-        matrix2.input();
-        System.out.println("Mult:");
+        matrix2.initRandom();
         Matrix matrixNew = matrix1.mult(matrix2);
-        matrixNew.print();
+        return matrixNew.zeit;
     }
 }
