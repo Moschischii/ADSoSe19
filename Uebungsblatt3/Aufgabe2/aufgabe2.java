987fe017de656fa1fabce8d4d37d99c64e9b11f9
Sebastian Geitner <sebastian.geitner@gmail.com>
2019-04-16 12:55:52 +0200

    Fertigstellung Uebungsblatt 3 Aufgabe 2
    - BubbleSort analog zur Vorlesung implementiert
    - SelectionSort analog zur Vorlesung implementiert

--- Uebungsblatt3/Aufgabe2/aufgabe2.java
+++ Uebungsblatt3/Aufgabe2/aufgabe2.java
@@ -6,12 +6,20 @@ public class aufgabe2 {
 
     public static void main(String[] args) {
         int a[] = {-5,13,-32,7,-3,17,23,12,-35,19};
+        int b[] = {-5,13,-32,7,-3,17,23,12,-35,19};
+        int c[] = {-5,13,-32,7,-3,17,23,12,-35,19};
 
-        int insert[] = insertionSort(a);
-        System.out.println("Ergebins Insert: " + Arrays.toString(insert));
+        insertionSort(a);
+        System.out.println("Ergebins Insert: " + Arrays.toString(a));
+
+        bubbleSort(b);
+        System.out.println("Ergebins Bubble: " + Arrays.toString(b));
+
+        selectionSort(c);
+        System.out.println("Ergebins Selection: " + Arrays.toString(c));
     }
 
-    private static int[] insertionSort(int a[]) {
+    private static void insertionSort(int a[]) {
         int n = a.length;
         int i, j, key;
 
@@ -28,7 +36,38 @@ public class aufgabe2 {
             a[i+1] = key;
             System.out.println("Array = " + Arrays.toString(a) + " bei Key = " + key);
         }
+    }
 
-        return a;
+    private static void bubbleSort(int a[]) {
+        int n = a.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n-2; j >= i; j--) {
+                System.out.println("Array = " + Arrays.toString(a));
+                if (a[j] > a[j+1]) {
+                    int h = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = h;
+                    System.out.println("Tausche " + h +" und " + a[j]);
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int a[]) {
+        int n = a.length;
+
+        int i, j, min;
+        for (i = 0; i < n; i++) {
+            min = i;
+            for (j = i; j < n; j++) {
+                if (a[j] < a[min])
+                    min = j;
+            }
+            int h = a[i];
+            a[i] = a[min];
+            a[min] = h;
+            System.out.println("Array = " + Arrays.toString(a) + " Tausche " + h +" und " + a[i]);
+        }
     }
 }
