cd513a6f4aea16d741c837f2e222d66862dca128
Sebastian Geitner <sebastian.geitner@gmail.com>
2019-05-07 10:40:17 +0200

    Aufgabe 3
    - Vorbereitungen zur Messung

---
+++ Uebungsblatt4/Aufgabe3/messung.java
@@ -0,0 +1,139 @@
+package Uebungsblatt4.Aufgabe3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class messung {
+
+    public static void main(String[] args) {
+
+        int[] a = {23,32,5,2,18};
+        int[] b = Arrays.copyOf(a,a.length);
+        int[] c = Arrays.copyOf(a,a.length);
+
+        countSort(a,(a.length-1),2);
+        System.out.println("Count: " + Arrays.toString(a));
+
+        mergeSort(b,0,(b.length-1));
+        System.out.println("Merge: " + Arrays.toString(b));
+
+        heapSort(c, 0, (c.length-1));
+        System.out.println("Heap: " + Arrays.toString(c));
+    }
+
+    /*
+     * Count Sort
+     */
+
+    private static void countSort(int[] a, int n, int smallest) {
+
+        int j = smallest;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(a[i]))
+                map.replace(a[i],(map.get(a[i])+1));
+            else
+                map.put(a[i],1);
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            while (!map.containsKey(j) || map.get(j)==0)
+                j++;
+
+            a[i]=j;
+            map.replace(j,(map.get(j)-1));
+        }
+    }
+
+    /*
+     * Heap Sort
+     */
+
+
+    private static void heapSort(int[] a, int f, int l) {
+        buildheap(a, f, l);
+        for (int i = l; i > f; i--) {
+            // swap
+            int old = a[f];
+            a[f] = a[i];
+            a[i] = old;
+
+            heapify(a, f, i-1, f);
+        }
+    }
+
+    private static void buildheap(int[] a, int f, int l) {
+        int n = l-f+1;
+        for (int i = (f + (n-2)/2); i >= f; i--) {
+            heapify(a, f, l ,i);
+        }
+    }
+
+    private static void heapify(int[] a, int f, int l, int root) {
+        int largest;
+        int left = f+(root-f)*2+1;
+        int right = f+(root-f)*2+2;
+
+        if (left <= l && a[left] > a[root])
+            largest = left;
+        else
+            largest = root;
+
+        if (right <= l && a[right] > a[largest])
+            largest = right;
+
+        if (largest != root) {
+            // swap
+            int old = a[root];
+            a[root] = a[largest];
+            a[largest] = old;
+
+            heapify(a, f, l, largest);
+        }
+    }
+
+    /*
+     * Merge Sort
+     */
+
+    private static void mergeSort(int[] a, int f, int l) {
+
+        if (f<l) {
+            int m = (f+l+1)/2;
+            mergeSort(a, f, m-1);
+            mergeSort(a, m, l);
+            merge(a, f, l, m);
+        }
+
+    }
+
+    private static void merge(int[] a, int f, int l, int m) {
+        int n = l-f+1;
+        int a1f = f;
+        int a1l = m-1;
+        int a2f = m;
+        int a2l = l;
+        int[] anew = new int[n];
+
+        for (int i = 0; i <n; i++) {
+            if (a1f <= a1l) {
+                if (a2f <= a2l) {
+                    if (a[a1f] <= a[a2f])
+                        anew[i] = a[a1f++];
+                    else
+                        anew[i] = a[a2f++];
+                }
+                else
+                    anew[i] = a[a1f++];
+            }
+            else
+                anew[i] = a[a2f++];
+        }
+
+        for (int i = 0; i < n; i++)
+            a[f+i] = anew[i];
+    }
+}
