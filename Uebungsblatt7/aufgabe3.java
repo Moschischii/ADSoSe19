9035af7ed3c754ae49dbb8665d8648bfb51e0613
Sebastian Geitner <sebastian.geitner@gmail.com>
2019-06-22 09:56:24 +0200

    Uebungsblatt 7
    Aufgabe 3
    - naive Suche

---
+++ Uebungsblatt7/aufgabe3.java
@@ -0,0 +1,27 @@
+package Uebungsblatt7;
+
+public class aufgabe3 {
+    public static void main(String[] args) {
+        char[] gesucht = "DATEN".toCharArray();
+        char[] gesamt = "ALGORITHMEN UND DATEN- STRUKTUREN".toCharArray();
+
+        naivaeSuche(gesucht, gesamt);
+    }
+
+    private static void naivaeSuche(char[] gesucht, char[] gesamt) {
+
+        int M = gesucht.length;
+        int N = gesamt.length;
+
+        for (int i = 0; i <= (N - M); i++) {
+            int j;
+            for (j = 0; j < M; j++) {
+                if (gesamt[j+i] != gesucht[j])
+                    break;
+            }
+
+            if (j == M)
+                System.out.println("Muster gefunden: Index: " + i);
+        }
+    }
+}
